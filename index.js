59de2422dc0928d578be8a922f224fdc8f2af136
VantaBlackzz <teliatenko@gmail.com>
2022-10-25 21:17:13 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Get the modal image actualize branch
+let modal = document.getElementById("myModal");
+let img = document.getElementById("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+let span = document.getElementsByClassName("close")[0];
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
